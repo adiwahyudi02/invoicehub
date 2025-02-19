2baaae9b14afe267a7cde4ab289a78d0a5a48d44
Adi Wahyudi <adi.whyd02@gmail.com>
2025-02-19 13:22:01 +0700

    feat: add navigations items constants

---
+++ src/constants/navigations.constans.tsx
@@ -0,0 +1,15 @@
+import { AddListIcon } from "@/components/ui/icons/add-list-icon";
+import { ListIcon } from "@/components/ui/icons/list-icon";
+
+export const navigationItems = [
+  {
+    label: "Add Invoice",
+    href: "/invoices/add",
+    icon: <AddListIcon />,
+  },
+  {
+    label: "My Invoice",
+    href: "/invoices/list",
+    icon: <ListIcon />,
+  },
+];
