d333d0bd9d63ac3cb2e74954acf46185d85741e6
Adi Wahyudi <adi.whyd02@gmail.com>
2025-02-19 13:06:43 +0700

    feat: create select-field component

---
+++ src/components/ui/select-field/index.tsx
@@ -0,0 +1,22 @@
+import { MenuItem } from "@mui/material";
+import { FieldValues } from "react-hook-form";
+import { TextField, TextFieldProps } from "../text-field";
+
+interface SelectFieldProps<T extends FieldValues> extends TextFieldProps<T> {
+  options: { value: string | number; label: string }[];
+}
+
+export const SelectField = <T extends FieldValues>({
+  options,
+  ...props
+}: SelectFieldProps<T>) => {
+  return (
+    <TextField<T> {...props} select>
+      {options.map(({ value, label }, index) => (
+        <MenuItem key={`${value}-${index}`} value={value}>
+          {label}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+};
