651cbab0b4ac7f361f976f075fa2f76e36c6a72e
Adi Wahyudi <adi.whyd02@gmail.com>
2025-02-19 13:00:17 +0700

    feat: create title component

---
+++ src/components/ui/title/index.tsx
@@ -0,0 +1,13 @@
+import { Typography, TypographyProps } from "@mui/material";
+
+interface TitleProps extends TypographyProps {
+  children: React.ReactNode;
+}
+
+export const Title: React.FC<TitleProps> = ({ children, ...props }) => {
+  return (
+    <Typography variant="h1" fontSize="1.625rem" fontWeight="700" {...props}>
+      {children}
+    </Typography>
+  );
+};
